e6460357e96a7ff245f2012da78c098652a2ef92
Sean <writeseanhong@gmail.com>
2019-09-20 23:51:15 -0500

    added reducers and combined using combineReducers function

---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'Little Wing', duration: '6:48' },
+        { title: 'Gravity', duration: '9:51' },
+        { title: 'Macarena', duration: '3:15' },
+        { title: 'I want it that way', duration: '1:45' }
+    ]
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong
+}
